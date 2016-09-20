4bc3a8f2b568ecfc766383abdeb1e11b9a356cef
Johann <jlau@stevens.edu>
2016-09-19 20:52:12 -0400

    Added duration

--- Exercise.java
+++ Exercise.java
@@ -14,16 +14,16 @@ public class Exercise {
 	private static final Random randomGenerator = new Random();	
 	
 	/** This is the constructor which was called Routine in the Assignment PDF */
-	public Exercise(ApparatusType at, Map <WeightPlateSize,Integer> weight){
+	public Exercise(ApparatusType at, Map <WeightPlateSize,Integer> weight,int duration){
 		this.at = at;
 		this.weight = weight;
-		this.duration = randomGenerator.nextInt(20);
+		this.duration = duration;	
 	}
 	/**
 	Generate Random Exercise given the max number of plates passed
 	
 	public Exercise generateRandom(Map<WeightPlateSize,Integer ){
-		
+		this.duration = randomGenerator.nextInt(20); 	
 	}
 	*/ 
 }
